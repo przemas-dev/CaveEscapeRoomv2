7726d6c6b5e90bf98740671fa1b09c64853aa7fb
przemas-dev <przemyslawreszka1@gmail.com>
2021-11-24 22:43:56 +0100

    add books, lab_scale scrpit and materials

---
+++ Assets/Scripts/LabScale.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabScale : MonoBehaviour
+{
+
+	public TextMesh TextMesh;
+	public Material MaterialScreenOn;
+	public Material MaterialScreenOff;
+	public MeshRenderer ScaleScreen;
+	[Tooltip("Measurement time given in frames")]
+	public int MeasurementTime = 20;
+
+	private string _screenText = "0.00";
+	private float _screenValue = 0.0f;
+	private float _value = 0.0f;
+	private int _remainingWeighingFrames = 0;
+	private bool _powerOn = true;
+	
+	void Start () {
+		
+	}
+
+	public void Switch()
+	{
+		_powerOn = !_powerOn;
+		ScaleScreen.material = _powerOn ? MaterialScreenOn : MaterialScreenOff;
+		ScreenUpdate();
+	}
+	public void ChangeMassOnScale(float mass)
+	{
+		_value = mass;
+		_remainingWeighingFrames = MeasurementTime;
+	}
+
+	private void ScreenUpdate()
+	{
+		_screenText = _screenValue.ToString("F2");
+		TextMesh.text = _powerOn ? _screenText : "";
+	}
+	void Update () {
+
+		if (_remainingWeighingFrames > 1)
+		{
+			_screenValue += (_value - _screenValue) * 0.3f;
+			_remainingWeighingFrames--;
+			ScreenUpdate();
+		}
+		else if(_remainingWeighingFrames == 1)
+		{
+			_screenValue = _value;
+			_remainingWeighingFrames--;
+			ScreenUpdate();
+		}
+	}
+}
---
+++ Assets/Scripts/LabScalePlate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabScalePlate : MonoBehaviour {
+
+    public LabScale LabScale;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		LabScale.ChangeMassOnScale(other.rigidbody.mass);
+	}
+
+	private void OnCollisionExit(Collision other)
+	{
+		LabScale.ChangeMassOnScale(0.0f);
+	}
+}
