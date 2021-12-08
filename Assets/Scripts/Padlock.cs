e9bf697eeecab9536792f991319a336c5bb0d06b
przemas-dev <62303978+przemas-dev@users.noreply.github.com>
2021-12-08 20:31:11 +0100

    fixing Padlock

---
+++ Assets/Scripts/Padlock.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Padlock : MonoBehaviour {
+
+	public int CipherCode;
+	public bool RandomDiskStartPos = true;
+	public Transform Shackle;
+	public Transform[] NumberDisks = new Transform[3];
+	public AudioClip[] PadlockSounds;
+	
+	private bool _isOpened = false;
+	private int[] _digits = new int[3];
+	private AudioSource _audioSource;
+	
+	void Start()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		if (RandomDiskStartPos)
+		{
+			var r = new System.Random();
+			SetDiskNumber(0, r.Next(9), false);
+			SetDiskNumber(1, r.Next(9), false);
+			SetDiskNumber(2, r.Next(9), false);
+		}
+	}
+	
+	private void SetDiskNumber(int diskNumber, int value, bool playSound = true)
+	{
+		_digits[diskNumber] = value;
+		SetDiskRotation(NumberDisks[diskNumber],value);
+		if (playSound) PlaySound();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetDiskRotation(Transform disk, int value)
+	{
+		disk.localRotation = Quaternion.Euler(0.0f, 0.0f, value * 36.0f);
+	}
+	
+	public void SpinDiskUp(int diskNumber)
+	{
+		if (_isOpened) return;
+		_digits[diskNumber] = (_digits[diskNumber] + 1) % 10;
+		SetDiskRotation(NumberDisks[diskNumber],_digits[diskNumber]);
+		PlaySound();
+		//TODO: animacja 
+		CheckNumbers();
+	}
+
+	public void SpinDiskDown(int diskNumber)
+	{
+		if (_isOpened) return;
+		_digits[diskNumber] = _digits[diskNumber] == 0 ? 9 : _digits[diskNumber] - 1;
+		SetDiskRotation(NumberDisks[diskNumber],_digits[diskNumber]);
+		PlaySound();
+		//TODO animacja
+		CheckNumbers();
+	}
+	private void CheckNumbers()
+	{
+		int code = _digits[0] * 100 + _digits[1] * 10 + _digits[2];
+		if (code == CipherCode) OpenPadlock();
+	}
+	
+	private void OpenPadlock()
+	{
+		var colliders = Shackle.GetComponentsInChildren<BoxCollider>();
+		Debug.Log("Padlock is open!");
+		foreach (var collider in colliders)
+		{
+			collider.enabled = false;
+		}
+
+		gameObject.AddComponent<Grabable>();
+		_isOpened = true;
+	}
+	
+	private void PlaySound()
+	{
+		_audioSource.clip = PadlockSounds[new Random().Next(PadlockSounds.Length - 1)];
+		_audioSource.Play();
+	}
+}
---
+++ Assets/Scripts/PadlockWheel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadlockWheel : MonoBehaviour,IInteractable
+{
+
+	public int WheelNumber;
+	public Padlock Padlock;
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool Interact()
+	{
+		Padlock.SpinDiskUp(WheelNumber);
+		return true;
+	}
+}
