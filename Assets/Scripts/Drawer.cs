c9085ba4b7971d97e05c8f277f809e192c3dd5ab
przemas-dev <62303978+przemas-dev@users.noreply.github.com>
2021-12-14 21:20:14 +0100

    fix drawer

--- Assets/Scripts/Drawer.cs
+++ Assets/Scripts/Drawer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.UI;
 
-public class Drawer : MonoBehaviour
+public class Drawer : MonoBehaviour, IGrabable
 {
 
 	public float MaxMove = 0.6f;
@@ -13,11 +13,18 @@ public class Drawer : MonoBehaviour
 	private Vector3 _startingPos;
 	private Vector3 _minPos;
 	private Vector3 _maxPos;
+
+	private Transform _grabberTransform;
+	private Vector3 _grabPosition;
+	private float _grabDistance;
+	private Rigidbody _rigidbody;
+	private Vector3 velocity;
+	private Vector3 _directionVector;
+	
 	void Start ()
 	{
-		var rigidbody = GetComponent<Rigidbody>();
-		rigidbody.centerOfMass = transform.localPosition;
-		rigidbody.inertiaTensorRotation = Quaternion.identity;
+		_rigidbody = GetComponent<Rigidbody>();
+		_rigidbody.drag = 1.0f;
 		_startingPos = transform.position;
 		switch (Direction)
 		{
@@ -25,29 +32,59 @@ public class Drawer : MonoBehaviour
 				throw new Exception("Select the axis of movement");
 				break;
 			case Axis.X:
-				_minPos = new Vector3(_startingPos.x - MaxMove, _startingPos.y, _startingPos.z);
-				_maxPos = new Vector3(_startingPos.x + MaxMove, _startingPos.y, _startingPos.z);
+				_directionVector=Vector3.right;
 				break;
 			case Axis.Y:
-				_minPos = new Vector3(_startingPos.x, _startingPos.y - MaxMove, _startingPos.z);
-				_maxPos = new Vector3(_startingPos.x, _startingPos.y + MaxMove, _startingPos.z);
+				_directionVector=Vector3.up;
 				break;
 			case Axis.Z:
-				_minPos = new Vector3(_startingPos.x, _startingPos.y, _startingPos.z - MaxMove);
-				_maxPos = new Vector3(_startingPos.x, _startingPos.y, _startingPos.z + MaxMove);
+				_directionVector=Vector3.back;
 				break;
 			default:
 				throw new ArgumentOutOfRangeException();
 		}
+		_maxPos = _startingPos + _directionVector * MaxMove;
+		_minPos=_startingPos + _directionVector * MaxMove *(-1);
 	}
 	
 	void Update ()
 	{
-		var diff = transform.position - _startingPos;
+		if (IsGrabbed)
+		{
+			_grabPosition = _grabberTransform.position + _grabberTransform.forward * _grabDistance;
+			velocity = (_grabPosition - transform.position) * 10.0f;
+			velocity.Scale(_directionVector);
+			_rigidbody.velocity = velocity;
+		}
 		if ((transform.position - _startingPos).magnitude > MaxMove)
 		{
 			if (transform.position.magnitude < _startingPos.magnitude) transform.position = _maxPos;
 			else transform.position = _minPos;
 		}
+	}
+	
+	public bool IsGrabbed { get; set; }
+	public void Grab(GameObject grabber)
+	{
+		IsGrabbed = true;
+		Debug.Log("grabbed");
+		_grabberTransform = grabber.transform;
+		_grabDistance = (transform.position - _grabberTransform.position).magnitude;
+	}
+
+	public void Release()
+	{
+		Debug.Log("released");
+		IsGrabbed = false;
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (IsGrabbed)
+		{
+			Gizmos.color=Color.black;
+			Gizmos.DrawSphere(_grabPosition,0.05f);
+		}
+		
 	}
 }
