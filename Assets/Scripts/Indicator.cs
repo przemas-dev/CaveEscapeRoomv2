ddc94cd631ec1521a4f0db7d47ff657d117a4cd0
TrylMik <64104285+TrylMik@users.noreply.github.com>
2021-12-22 00:39:10 +0100

    script and materials for indicators

---
+++ Assets/Scripts/Indicator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Indicator : MonoBehaviour {
+
+	public GameObject tube;
+	public Material material;
+	private MeshRenderer rend;
+	private NetworkView nv;
+
+
+	// Use this for initialization
+	void Start () {
+		nv = GetComponent<NetworkView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject == tube)
+		{
+			rend = tube.GetComponent<MeshRenderer>();
+			ChangeMaterial(rend);
+		}
+	}
+
+	public void ChangeMaterial(Renderer rend)
+	{
+		nv.RPC("ChangeMaterialRPC", RPCMode.OthersBuffered, rend);
+		rend.material = material;
+	}
+
+    [RPC]
+    public void ChangeMaterialRPC(Renderer rend)
+    {
+        rend.material = material;
+    }
+}
